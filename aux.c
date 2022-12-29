290c48c841cc6fd3bd8db868f01cdfbb51e1b76b
pedperei <pedrob1095@hotmail.com>
2022-12-29 20:08:46 +0000

    exit fix2

--- aux.c
+++ aux.c
@@ -6,7 +6,7 @@
 /*   By: pedperei <pedperei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/25 23:01:09 by pedperei          #+#    #+#             */
-/*   Updated: 2022/12/29 19:59:31 by pedperei         ###   ########.fr       */
+/*   Updated: 2022/12/29 20:08:19 by pedperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,7 @@
 int	check_if_game_finished(t_solong *so_long, int pos_l, int pos_c)
 {
 	if (so_long->map->game_map[pos_l][pos_c] == 'C')
-	{
-		so_long->map->game_map[pos_l][pos_c] = '0';
 		so_long->map->count_collect--;
-	}
 	else if (so_long->map->game_map[pos_l][pos_c] == 'E'
 			&& so_long->map->count_collect > 0)
 		return (0);
